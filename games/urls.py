ddefb932cdbb1384c510a4f5d2e37bbc16cd207a
chrry03 <ocr925@dgu.ac.kr>
2026-01-19 00:04:30 +0900

    games앱의 urls.py 활성화 몇개 하고(주석을 풀음), base.html 최종 완료

--- games/urls.py
+++ games/urls.py
@@ -7,9 +7,12 @@ urlpatterns = [
     path('', views.main_view, name='main'),
     
     # [과제 요구사항에 맞춘 URL 구조 예시 - 나중에 주석 풀고 쓰세요]
-    # [cite_start]path('list/', views.game_list_view, name='game_list'), # 게임 목록
-    # [cite_start]path('ranking/', views.ranking_view, name='ranking'),  # 랭킹 페이지
-    # [cite_start]path('attack/', views.attack_view, name='attack'),     # 공격하기(게임 생성)
+    # [cite_start]
+    path('list/', views.game_list_view, name='game_list'), # 게임 목록
+    # [cite_start]
+    path('ranking/', views.ranking_view, name='ranking'),  # 랭킹 페이지
+    # [cite_start]
+    path('attack/', views.attack_view, name='game_attack'),     # 공격하기(게임 생성)
     # path('ranking/', views.ranking_list, name='ranking'), # 랭킹 페이지 주소 설정 [cite: 80]
     # 반격하기 처리 주소 (게임 ID를 넘겨받아야 함)
     # path('counter/<int:game_id>/', views.counter_attack, name='counter_attack'),
