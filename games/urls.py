f4accd671333d010456c24436308943d8f9f164b
YanggalleDolmery <gamaen9183@gmail.com>
2026-01-18 20:59:43 +0900

    백엔드3 구현

--- games/urls.py
+++ games/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     # [cite_start]path('list/', views.game_list_view, name='game_list'), # 게임 목록
     # [cite_start]path('ranking/', views.ranking_view, name='ranking'),  # 랭킹 페이지
     # [cite_start]path('attack/', views.attack_view, name='attack'),     # 공격하기(게임 생성)
+    # path('ranking/', views.ranking_list, name='ranking'), # 랭킹 페이지 주소 설정 [cite: 80]
+    # 반격하기 처리 주소 (게임 ID를 넘겨받아야 함)
+    # path('counter/<int:game_id>/', views.counter_attack, name='counter_attack'),
 ]
