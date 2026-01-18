fbebd610abbae6e492e68fe0970fee09a9cce00f
seonwoo <sunnn62@naver.com>
2026-01-18 17:42:28 +0900

    Feat: 회원가입/로그인/로그아웃 로직 구현 및 템플릿 추가

--- users/views.py
+++ users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-# Create your views here.
+# 1. 회원가입
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # 가입하자마자 자동 로그인
+            return redirect('users:login') # 일단 로그인 페이지로 보냄 (나중에 메인으로 수정)
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/signup.html', {'form': form})
+
+# 2. 로그인
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('/') # 메인 페이지로 이동 (지금은 에러날 수 있음)
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+# 3. 로그아웃
+def logout_view(request):
+    logout(request)
+    return redirect('users:login')
