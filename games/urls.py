2c65fe75ae44ce086199c48854f76da0da6454dc
YanggalleDolmery <gamaen9183@gmail.com>
2026-01-19 23:21:25 +0900

    로그인 디버깅, 게임로직 구현

--- games/urls.py
+++ games/urls.py
@@ -4,30 +4,19 @@ from . import views
 app_name = 'games'
 
 urlpatterns = [
-    # 1. 메인 페이지 (main.html / game_list.html)
+    # 1. 메인 페이지 (로그인 여부에 따라 '대문' 또는 '게임리스트'를 보여줍니다)
     path('', views.main_view, name='main'),
-    path('loginedmain/', views.mainlogined_view, name='logined_main'),
-    path('preview/logined/', views.mainlogined_view),
-   
-    # 로그인, 회원가입 (views.py에 해당 함수가 정의되어 있어야 합니다)
+    
+    # 2. 유저 관련 (로그인, 회원가입)
     path("login/", views.login_view, name="login"),
     path("signup/", views.signup_view, name="signup"),
 
-    # 2. 공격하기 (game_attack.html)
+    # 3. 게임 기능 (공격, 반격, 결과, 랭킹)
     path('attack/', views.attack_view, name='game_attack'),
-
-    # 3. 게임 상세 (game_detail.html)
-    path('detail/<int:pk>/', views.game_detail_view, name='game_detail'),
-
-    # 4. 반격하기
     path('counter/<int:game_id>/', views.counter_attack, name='counter_attack'),
-
-    # 5. 랭킹 (ranking.html)
+    path('detail/<int:pk>/', views.game_detail_view, name='game_detail'),
     path('ranking/', views.ranking_list, name='ranking'),
-
-    # [핵심] 게임 상태 확인용 URL (이 부분이 있어야 로딩화면에서 에러가 안 납니다!)
-    path('check_status/<int:game_id>/', views.check_game_status, name='check_game_status'),
     
-    # 6. 전적 목록(game_list.html)
-    path('gamelist/', views.game_list_view, name='game_list'),
+    # 4. API (상태 확인용)
+    path('check_status/<int:game_id>/', views.check_game_status, name='check_game_status'),
 ]
