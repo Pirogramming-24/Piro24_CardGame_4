423936d72f092f956316db69b1af16c023ace063
seonwoo <sunnn62@naver.com>
2026-01-19 11:04:46 +0900

    Feat: 유저 닉네임 추가 및 게임 모델 마이그레이션 업데이트

--- users/models.py
+++ users/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     points = models.IntegerField(default=0)
-    
+    nickname = models.CharField(max_length=30, null=True, blank=True)
     def __str__(self):
         return self.username
